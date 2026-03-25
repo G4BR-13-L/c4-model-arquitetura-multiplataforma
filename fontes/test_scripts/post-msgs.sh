7402fe7a559dbf3ef11e54ff62b87c18a1869ed1
G4BR-13-L <gabriel.victorc13@gmail.com>
2026-03-24 22:23:45 -0300

    Dockerfile refinado

--- fontes/test_scripts/post-msgs.sh
+++ fontes/test_scripts/post-msgs.sh
@@ -3,6 +3,30 @@
 # Se a fila for FIFO, lembre-se do --message-group-id
 QUEUE_URL="http://localhost:4566/000000000000/notification_send_email.fifo"
 
+echo "--- 1. Teste de fogo do trabalho---"
+# Ajustado para garantir que a interpolação do bash não quebre o JSON
+NOW=$(date +%s)
+TESTE='{
+   "event_type":"notification.email",
+   "occurred_at":"2026-03-25T00:53:42.334205Z",
+   "data":{
+      "sender_email":"no-reply@vehicle-service.com",
+      "sender_name":"Vehicle Service",
+      "recipient_email":"email@email.com",
+      "recipient_name":"usuario",
+      "subject":"Ve\u00EDculo Volkswagen Gol com placa ABC-1234 reservado.",
+      "content":"{\n  \u0022Id\u0022: \u002261351d1c-86d5-4b78-80df-c78c7c991adc\u0022,\n  \u0022Model\u0022: \u0022Volkswagen Gol\u0022,\n  \u0022LicensePlate\u0022: \u0022ABC-1234\u0022\n}"
+   }
+}'
+
+# Alterado: localstack_main -> localstack
+docker exec -t localstack awslocal sqs send-message \
+    --queue-url "$QUEUE_URL" \
+    --message-body "$TESTE" \
+    --message-group-id "emails-notificacao" \
+    --message-deduplication-id "msg-$NOW"
+
+
 echo "--- 1. Enviando mensagem CORRETA (Novo Formato) ---"
 # Ajustado para garantir que a interpolação do bash não quebre o JSON
 NOW=$(date +%s)
@@ -25,6 +49,9 @@ docker exec -t localstack awslocal sqs send-message \
     --message-body "$EMAIL_CORRETO" \
     --message-group-id "emails-notificacao" \
     --message-deduplication-id "msg-$NOW"
+
+
+
 
 echo -e "\n--- 2. Enviando mensagem com CAMPOS FALTANDO ---"
 EMAIL_INCOMPLETO='{
